3804bef57354980188b1991658937d0d58700287
gomezlucas <gomez-lucas@live.com.ar>
2024-02-28 00:48:49 -0300

    feat: starting integration with api, object data set

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const baseUrl = "https://stagingapi.thelogisticsworld.com/v1/events/register";
+
 function validateEmail(email) {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return regex.test(email);
@@ -57,6 +59,45 @@ function checkButton(isValid, isEqual) {
   }
 }
 
+// Función para enviar la Data
+async function postData(url, data) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Set appropriate content type
+      },
+      body: JSON.stringify(data), // Convert object to JSON string for body
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseData = await response.json(); // Parse response as JSON if needed
+    console.log(responseData);
+    return responseData; // Return the response data (optional)
+  } catch (error) {
+    console.error("Error:", error);
+    // Handle the error appropriately
+  }
+}
+
+// Función para obtener los UTMs 
+function getUTMParams() {
+  const url = window.location.href;
+
+  const queryString = new URL(url).searchParams;
+
+  const utmParams = {};
+
+  for (const [key, value] of queryString.entries()) {
+     utmParams[key] = value;
+  }
+
+  return utmParams;
+}
+
 // Handle para el navbar Mobile
 /*
 function openNavbar(navbarButton) {
@@ -85,7 +126,7 @@ document.addEventListener("DOMContentLoaded", function () {
     isValid = validateEmail(emailInput.value);
     showValidationResult(emailInput, isValid);
     if (emailConfirmationInput.value !== "") {
-      console.log("entro")
+      console.log("entro");
       isEqual = validateEmailConfirmation(emailInput, emailConfirmationInput);
     }
   });
@@ -101,37 +142,94 @@ document.addEventListener("DOMContentLoaded", function () {
    }
 
    */
-/*
+  /*
   navbarButton.addEventListener("click", (e) => {
     openNavbar(navbarButton);
   });
 */
 
+ 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     const submitButton = document.getElementById("submitButton");
     const step1 = document.querySelector(".register_step-1");
-    const stepOk = document.querySelector(".register_step-ok");
+    const stepCreated = document.querySelector(".register_step-created");
+    const stepRegistered = document.querySelector(".register_step-registered");
     const languageWrapper = document.querySelector(".language_wrapper_wrapper");
+    const emailConfirmationValue = document.getElementById("emailConfirmationInput").value;
+    const userEmail = document.getElementById("user_email")
+    userEmail.textContent = emailConfirmationValue
+    const userAgent = navigator;
 
-    submitButton.classList.add("disabled");
+
+     submitButton.classList.add("disabled");
     submitButton.textContent = "Enviando...";
+      
+    const {utm_source,utm_medium, utm_campaign,utm_term, utm_content} = getUTMParams();
+ 
+    console.log(userAgent, 'the user agent is')
 
+    const data = {
+      email: emailConfirmationValue,
+      utm_source,
+      utm_medium,
+      utm_campaign,
+      utm_term,
+      utm_content,
+      device: "mobile",
+      event_type_id: 1,
+    };
+    console.log(data)
+
+
+/*
+    postData(baseUrl, data)
+      .then((responseData) => {
+        if (responseData) {
+          step1.classList.add("closing");
+          step1.addEventListener(
+            "animationend",
+            () => {
+              step1.classList.add("hide");
+              languageWrapper.classList.add("hide");
+              step1.classList.remove("closing");
+              if (responseData.msj == "Usuario creado. Se ha enviado un mail de validación"){
+                stepCreated.classList.add("show");
+              }else{
+                stepRegistered.classList.add("show");
+              }
+            },
+            { once: true }
+          );
+        } else {
+          console.log("Data submitted successfully.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error submitting data:", error);
+      });
+*/
+ 
+    const responseData = "Usuario crado. Se ha enviado un mail de validación"
     setTimeout(() => {
-      submitButton.classList.remove("disabled");
-      submitButton.textContent = "INICIAR REGISTRO";
+      //   submitButton.classList.remove("disabled");
+      //  submitButton.textContent = "INICIAR REGISTRO";
       step1.classList.add("closing");
-
       step1.addEventListener(
         "animationend",
         () => {
           step1.classList.add("hide");
           languageWrapper.classList.add("hide");
           step1.classList.remove("closing");
-          stepOk.classList.add("show");
+          if (responseData == "Usuario creado. Se ha enviado un mail de validación"){
+            stepCreated.classList.add("show");
+          }else{
+            stepRegistered.classList.add("show");
+          }
         },
         { once: true }
       );
+
     }, 1000);
   });
 });
